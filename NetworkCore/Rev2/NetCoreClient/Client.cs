a48e343841a82085ac1bda1722cc59e2e3741c69
Tobias Hattinger <tobias.hattinger@gmx.at>
2019-10-18 07:27:33 +0200

    Finished RabbitMQ-Test

--- NetworkCore/Rev2/NetCoreClient/Client.cs
+++ NetworkCore/Rev2/NetCoreClient/Client.cs
@@ -1,4 +1,5 @@
 ﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,9 +13,39 @@ namespace NetCoreClient
         static void Main(string[] args)
         {
             var factory = new ConnectionFactory();
-
-
             factory.HostName = "localhost";
+
+            using (var con = factory.CreateConnection())
+            {
+                using(var channel = con.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: "hallo",
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null
+                        );
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += Consumer_Received;
+
+                    channel.BasicConsume(
+                        queue: "hallo",
+                        autoAck: true,
+                        consumer: consumer
+                        );
+
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            var body = e.Body;
+            var message = Encoding.UTF8.GetString(body);
+
+            Console.WriteLine("Received message: {0}", message);
         }
     }
 }
