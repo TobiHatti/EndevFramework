09956d874f464b0afdb7ccd3fc2b4b5bde2d192a
Tobias Hattinger <tobias.hattinger@gmx.at>
2019-10-21 15:35:35 +0200

    Finished first working setup
    Wuhu \^-^/

---
+++ NetworkCore/Rev1/MultiUserServer_Simplified/Server.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiUserServer_Simplified
+{
+    class Server
+    {
+        private static readonly Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static readonly List<Socket> clientSockets = new List<Socket>();
+        private const int BUFFER_SIZE = 2048;
+        private const int PORT = 100;
+        private static readonly byte[] buffer = new byte[BUFFER_SIZE];
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Server";
+
+            // Server Setup
+            Console.WriteLine("Setting up server...");
+            serverSocket.Bind(new IPEndPoint(IPAddress.Any, PORT));
+            serverSocket.Listen(0);
+            serverSocket.BeginAccept(AcceptCallback, null);
+            Console.WriteLine("Server setup complete");
+
+            // Send to client
+            while (true)
+            {
+                if(clientSockets.Count > 0)
+                {
+                    Socket current = clientSockets[0];
+
+                    byte[] data = Encoding.ASCII.GetBytes(Console.ReadLine());
+                    current.Send(data);
+                    Console.WriteLine("Message Sent!");
+                }
+
+                if (clientSockets.Count > 1)
+                {
+                    string tmp = Console.ReadLine();
+
+                    Socket current = clientSockets[0];
+                    byte[] data = Encoding.ASCII.GetBytes(tmp);
+                    current.Send(data);
+
+                    current = clientSockets[1];
+                    data = Encoding.ASCII.GetBytes(tmp);
+                    current.Send(data);
+
+                    Console.WriteLine("Broadcast Sent!");
+                }
+            }
+
+
+            // When we press enter close everything
+            Console.ReadLine();
+
+            foreach (Socket socket in clientSockets)
+            {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+
+            serverSocket.Close();
+        }
+
+        private static void AcceptCallback(IAsyncResult AR)
+        {
+            Socket socket;
+
+            try
+            {
+                socket = serverSocket.EndAccept(AR);
+            }
+            catch (ObjectDisposedException) // I cannot seem to avoid this (on exit when properly closing sockets)
+            {
+                return;
+            }
+
+            clientSockets.Add(socket);
+            socket.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallback, socket);
+            Console.WriteLine("Client connected, waiting for request...");
+            serverSocket.BeginAccept(AcceptCallback, null);
+        }
+
+        private static void ReceiveCallback(IAsyncResult AR)
+        {
+            Socket current = (Socket)AR.AsyncState;
+            int received;
+
+            try
+            {
+                received = current.EndReceive(AR);
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Client forcefully disconnected");
+                // Don't shutdown because the socket may be disposed and its disconnected anyway.
+                current.Close();
+                clientSockets.Remove(current);
+                return;
+            }
+
+            byte[] recBuf = new byte[received];
+            Array.Copy(buffer, recBuf, received);
+            string text = Encoding.ASCII.GetString(recBuf);
+            Console.WriteLine("Received Text: " + text);
+
+            /*
+            if (text.ToLower() == "exit") // Client wants to exit gracefully
+            {
+                // Always Shutdown before closing
+                current.Shutdown(SocketShutdown.Both);
+                current.Close();
+                clientSockets.Remove(current);
+                Console.WriteLine("Client disconnected");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Text is an invalid request");
+                byte[] data = Encoding.ASCII.GetBytes("Invalid request");
+                current.Send(data);
+                Console.WriteLine("Warning Sent");
+            }
+
+            */
+
+            current.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallback, current);
+            
+        }
+    }
+}
