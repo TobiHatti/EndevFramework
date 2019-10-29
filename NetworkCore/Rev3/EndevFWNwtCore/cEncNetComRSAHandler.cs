0f3daa44f807a64c84443aad64b11ee84ecd9353
Tobias Hattinger <tobias.hattinger@gmx.at>
2019-10-29 16:11:08 +0100

    Added and completed RSA-Handler

--- NetworkCore/Rev3/EndevFWNwtCore/cEncNetComRSAHandler.cs
+++ NetworkCore/Rev3/EndevFWNwtCore/cEncNetComRSAHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -28,6 +29,64 @@ namespace EndevFWNwtCore
     /// </summary>
     public class RSAHandler
     {
-       
+        private static char RSAByteDelimiter = '-';
+
+        /// <summary>
+        /// Generates a unique key-pair for RSA-encryption
+        /// </summary>
+        /// <returns>A Private/Public Key-Pair</returns>
+        public static RSAKeyPair GenerateKeyPair()
+        {
+            RSAKeyPair keys = new RSAKeyPair();
+            RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+            keys.PublicKey = RSA.ToXmlString(false);
+            keys.PrivateKey = RSA.ToXmlString(true);
+
+            return keys;
+        }
+
+        /// <summary>
+        /// Encrypts a string using a public RSA-key
+        /// </summary>
+        /// <param name="pPartnerPublicKey">Public key of the recepiant</param>
+        /// <param name="pData">Data to be encrypted</param>
+        /// <returns>The encrypted data-string</returns>
+        public static string Encrypt(string pPartnerPublicKey, string pData)
+        {
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            rsa.FromXmlString(pPartnerPublicKey);
+            byte[] dataToEncrypt = Encoding.Unicode.GetBytes(pData);
+            byte[] encryptedByteArray = rsa.Encrypt(dataToEncrypt, false).ToArray();
+            int length = encryptedByteArray.Count();
+            int item = 0;
+            StringBuilder sb = new StringBuilder();
+            foreach (byte x in encryptedByteArray)
+            {
+                item++;
+                sb.Append(x);
+                if (item < length) sb.Append(RSAByteDelimiter);
+            }
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Decrypts a RSA-encrypted string using a private-key
+        /// </summary>
+        /// <param name="pLocalPrivateKey">Private-key of the user (recepiant)</param>
+        /// <param name="pData">RSA-encrypted data-string</param>
+        /// <returns>The decrypted string</returns>
+        public static string Decrypt(string pLocalPrivateKey, string pData)
+        {
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            string[] dataArray = pData.Split(new char[] { RSAByteDelimiter });
+            byte[] dataByte = new byte[dataArray.Length];
+            for (int i = 0; i < dataArray.Length; i++)
+            {
+                dataByte[i] = Convert.ToByte(dataArray[i]);
+            }
+            rsa.FromXmlString(pLocalPrivateKey);
+            byte[] decryptedByte = rsa.Decrypt(dataByte, false);
+            return Encoding.Unicode.GetString(decryptedByte);
+        }
     }
 }
