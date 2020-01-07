7ea61f8406514a8bc958dc776c59e00e8c939f07
Tobias Hattinger <tobias.hattinger@gmx.at>
2020-01-07 13:21:17 +0100

    Added Startup-Assist

--- NetworkCore/NetComRMQ/RMQSimulation/SimServer.Designer.cs
+++ NetworkCore/NetComRMQ/RMQSimulation/SimServer.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
@@ -49,14 +49,14 @@
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 1;
             // 
-            // button1
+            // btnLogin
             // 
-            this.button1.Location = new System.Drawing.Point(45, 66);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnLogin.Location = new System.Drawing.Point(45, 66);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(100, 23);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Log In";
+            this.btnLogin.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
@@ -80,10 +80,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(164, 102);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -104,7 +104,7 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
     }
---
+++ NetworkCore/NetComRMQ/SampleStartupAssist/cStartup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SampleStartupAssist
+{
+    class Startup
+    {
+#pragma warning disable IDE0060 // unused arguments
+        static void Main(string[] args)
+        {
+            var clientPath = @"..\..\..\RMQSampleClient\bin\Debug\RMQSampleClient.exe";
+            var serverPath = @"..\..\..\RMQSampleServer\bin\Debug\RMQSampleServer.exe";
+
+            
+            
+            Process.Start(serverPath);
+
+
+            while (true)
+            {
+                Console.Write("Amount of clients: ");
+                var clientCount = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 0; i < clientCount; i++)
+                {
+                    Process.Start(clientPath, $"{i + 1}");
+                    Thread.Sleep(500);
+                }
+            }
+
+#pragma warning disable 0162 // unreachable code
+            Environment.Exit(0);
+#pragma warning restore 0162 // unreachable code
+        }
+#pragma warning restore IDE0060 // unused arguments
+    }
+}
